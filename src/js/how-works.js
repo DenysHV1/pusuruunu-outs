66dbe0ff2e9df734765c58b901a1426321a00457
DenVik <denyagarkusha2@gmail.com>
2025-04-19 10:57:56 +0200

    how-works

---
+++ src/js/how-works.js
@@ -0,0 +1,43 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+export const howWorksSwiper = () => {
+	let howWorksSwiper = null;
+
+	const initHowWorksSwiper = () => {
+		if (window.innerWidth < 1440) {
+			if (!howWorksSwiper) {
+				howWorksSwiper = new Swiper(".swiper-how-works", {
+					slidesPerView: 1,
+					slidesPerGroup: 1,
+centeredSlides: false,
+					loop: false,
+					grabCursor: true,
+					spaceBetween: 24,
+					speed: 800,
+					simulateTouch: true,
+					touchRatio: 1,
+					mousewheel: {
+						sensitivity: 0.5,
+					},
+					keyboard: {
+						enabled: true,
+						onlyInViewport: true,
+					},
+					pagination: {
+						el: ".swiper-pagination",
+						clickable: true,
+					},
+				});
+			}
+		} else {
+			if (howWorksSwiper) {
+				howWorksSwiper.destroy(true, true);
+				howWorksSwiper = null;
+			}
+		}
+	};
+
+	initHowWorksSwiper();
+	window.addEventListener("resize", initHowWorksSwiper);
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import mobileMenu from './js/header.js';
 import { cookiePolicy } from './js/cookiesPolicy.js';
+import { howWorksSwiper } from './js/how-works.js';
 
 mobileMenu();
 cookiePolicy();
+howWorksSwiper();
